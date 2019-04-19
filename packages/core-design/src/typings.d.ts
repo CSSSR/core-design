38a6d0c00762fb8e792a1abdb0d76c25ce2856b2
Alexander Simonov <palmflip@gmail.com>
2019-04-19 15:36:47 +0300

    chore: CORE-11 Добавил typings.d.ts чтобы TS не ругался при импорте assets

---
+++ packages/core-design/src/typings.d.ts
@@ -0,0 +1,24 @@
+declare module '*.woff' {
+  const woff: string
+  export default woff
+}
+
+declare module '*.woff2' {
+  const woff2: string
+  export default woff2
+}
+
+declare module '*.eot' {
+  const eot: string
+  export default eot
+}
+
+declare module '*.ttf' {
+  const ttf: string
+  export default ttf
+}
+
+declare module '*.otf' {
+  const otf: string
+  export default otf
+}
