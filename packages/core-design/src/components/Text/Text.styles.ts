e04a9edf3132ab6b35a99f7497522159573b30f0
Artyom Resh <artyomresh@gmail.com>
2019-04-22 10:29:47 +0300

    chore(*): CORE-23 Создал компонент Text

---
+++ packages/core-design/src/components/Text/Text.styles.ts
@@ -0,0 +1,9 @@
+import { font_p16_regular, font_roboto_slab_light, font_p24_strong } from '../../styles/fonts'
+
+export default {
+  font: {
+    font_p16_regular,
+    font_roboto_slab_light,
+    font_p24_strong,
+  }
+}
