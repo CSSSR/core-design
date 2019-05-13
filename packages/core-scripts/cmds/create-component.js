006fc5a1d6604196e0469454d4f36273e501f7d9
Artyom Resh <artyomresh@gmail.com>
2019-05-13 11:59:52 +0300

    refactor(templates): CORE-35
    — перенес темлейты для генерации компонентов в  формат hbs и пместил в фолдер templates;
    — написал валидацию для названия создаваемого компонента;
    — поправил код шаблона для компонентов.

--- packages/core-scripts/cmds/create-component.js
+++ packages/core-scripts/cmds/create-component.js
@@ -1,47 +1,38 @@
 const fs = require('fs-extra')
 const path = require('path')
+const { compile } = require('handlebars')
 
 function handler(argv) {
   const componentName = argv.name
-  const componentPath = path.format({ dir: `components/${componentName}`, name: componentName, ext: '.tsx' })
-  const componentStylesPath = path.format({ dir: `components/${componentName}`, name: componentName, ext: '.styles.ts' })
 
-  const componentTemplate = `
-  import * as React from 'react'
-  import styled from '@emotion/styled'
-  import styles from './${componentName}.styles'
-  // import { Название_компонента_из_нашей_библиотеки_компонентов } from '@asmy/core-design'
-
-  const Origin${componentName} = props =>
-    <div>
-      { props.children }
-    </div>
-
-  const ${componentName} = styled(Origin${componentName})\`
-    \${styles.base}
-  \`
-
-  export { ${componentName} }
-  export default ${componentName}
-  `
-
-  const componentStylesTemplate = `
-  import { css } from '@emotion/core'
-
-  export default {
-    base: css\`
-      display: block;
-    \`,
+  if (!componentName) {
+    console.log('\n-----------------------')
+    console.log('\n\x1b[31m', 'Введите название компонента', '\x1b[0m')
+    console.log('\n-----------------------')
+    throw ''
   }
-  `
 
-  fs.outputFile( componentPath, componentTemplate, (err) => {
+  if (componentName[0] !== componentName[0].toUpperCase()) {
+    console.log('\n-----------------------')
+    console.log('\n\x1b[31m', 'Название компонента должно начинаться с заглавной буквы и быть написано в PascalCase', '\x1b[0m')
+    console.log('\n-----------------------')
+    throw ''
+  }
+
+  const componentTemplatePath = path.resolve(__dirname, '../templates/components/Component', `Component.js.hbs`)
+  const componentStylesTemplatePath = path.resolve(__dirname, '../templates/components/Component', `Component.styles.js.hbs`)
+  const componentTemplate = compile(fs.readFileSync(componentTemplatePath, { encoding: 'utf8' }))
+  const componentStylesTemplate = compile(fs.readFileSync(componentStylesTemplatePath, { encoding: 'utf8' }))
+  const componentPath = path.format({ dir: `components/${componentName}`, name: componentName, ext: '.js' })
+  const componentStylesPath = path.format({ dir: `components/${componentName}`, name: componentName, ext: '.styles.js' })
+
+  fs.outputFile( componentPath, componentTemplate({componentName}), (err) => {
     if (err) { throw err }
     console.log('\n-----------------------')
     console.log('Файл компонента был успешно создан! 🎉')
   })
 
-  fs.outputFile( componentStylesPath, componentStylesTemplate, (err) => {
+  fs.outputFile( componentStylesPath, componentStylesTemplate(), (err) => {
     if (err) { throw err }
 
     console.log('Файл стилей для компонента был успешно создан! 🎉')
@@ -50,9 +41,6 @@ function handler(argv) {
 }
 
 exports.command = 'create-component [name]'
-
 exports.desc = 'Создать новый компонент'
-
 exports.builder = {}
-
 exports.handler = handler
