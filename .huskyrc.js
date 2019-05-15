b65908f1e00ca7a5ca81b77324c7e2a697e74d9b
Alexander Simonov <palmflip@gmail.com>
2019-05-15 10:14:11 +0300

    feat(core-docs): CORE-48 Поднял версию next + добавил хук на prepush

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   hooks: {
     'pre-commit': 'lerna run --concurrency 1 --stream precommit',
+    'pre-prepush': 'lerna run --concurrency 1 --stream prepush',
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   },
 }
