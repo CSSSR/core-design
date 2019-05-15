6e3d073addffbb1b545b2940c07c18d7bea070a8
Alexander Simonov <palmflip@gmail.com>
2019-05-15 10:16:42 +0300

    feat(core-docs): CORE-48 Убрал prepush хук #2

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   hooks: {
     'pre-commit': 'lerna run --concurrency 1 --stream precommit',
-    'pre-prepush': 'lerna run --concurrency 1 --stream prepush',
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   },
 }
