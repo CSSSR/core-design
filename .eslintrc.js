9b537123241a183353eae1f9ab27bbeb75a175c6
Artyom Resh <artyomresh@gmail.com>
2019-07-11 17:36:19 +0300

    fix(linting): CORE-67 вынес настройки prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,5 @@ module.exports = {
     node: true,
     es6: true,
   },
-  rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        useTabs: false,
-        printWidth: 100,
-        semi: false,
-        singleQuote: true,
-        jsxSingleQuote: false,
-        trailingComma: 'all',
-        bracketSpacing: true,
-      },
-    ],
-  },
   extends: '@csssr/eslint-config-core',
 }
