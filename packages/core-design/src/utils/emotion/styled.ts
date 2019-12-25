58c0b441b23b5987bcaac7e3059b55bb6648220c
Artyom Resh <artyomresh@gmail.com>
2019-12-25 18:55:55 +0300

    chore: обновляет зависимости

--- packages/core-design/src/utils/emotion/styled.ts
+++ packages/core-design/src/utils/emotion/styled.ts
@@ -1,5 +1,5 @@
 // tslint:disable-next-line: no-implicit-dependencies
-import styled, { CreateStyled } from '@original-emotion/styled'
+import styled, { CreateStyled } from '@emotion/styled'
 import { Theme } from '../../themes/types'
 
 export default styled as CreateStyled<Theme>
