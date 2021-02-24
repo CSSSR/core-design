60df64578ccbfea8c9531d314598960f654e01e4
ArtyomResh <artyom.resh@csssr.com>
2021-02-24 17:13:44 +0300

    fix(components): COM-3219 update CookiesPopup
    - add data-testid
    - add new props
    - update component story

--- packages/core-design/src/data/cookiesPopupPresets.ts
+++ packages/core-design/src/data/cookiesPopupPresets.ts
@@ -1,28 +1,47 @@
 import cookiesPopupLinks from './cookiesPopupLinks'
 
+const dataTestIds = {
+  blocks: {
+    main: 'CookiesPopup::block',
+  },
+  buttons: {
+    close: 'CookiesPopup:button.close',
+  },
+  links: {
+    privacyPolicy: 'CookiesPopup:link.privacyPolicy',
+    cookiesPolicy: 'CookiesPopup:link.cookiesPolicy',
+  },
+}
+
 const presets = {
   defaultEn: {
     text: cookiesPopupLinks.en.text,
     links: [
       {
+        id: cookiesPopupLinks.en.links[0].id,
         title: cookiesPopupLinks.en.links[0].title,
         href: cookiesPopupLinks.en.links[0].href,
       },
       {
+        id: cookiesPopupLinks.en.links[1].id,
         title: cookiesPopupLinks.en.links[1].title,
         href: cookiesPopupLinks.en.links[1].href,
       },
     ],
+    dataTestIds,
   },
   defaultRu: {
     text: cookiesPopupLinks.ru.text,
     links: [
       {
+        id: cookiesPopupLinks.ru.links[0].id,
         title: cookiesPopupLinks.ru.links[0].title,
         href: cookiesPopupLinks.ru.links[0].href,
       },
     ],
+    dataTestIds,
   },
 }
 
+export { dataTestIds }
 export default presets
