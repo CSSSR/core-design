c65e2c85b9405e1f8445bad962f84ea7ce90b2e6
Viktor Kosinov <vkosinov@ya.ru>
2021-04-12 16:37:23 +0300

    feat(components): COM-3432 Add data-testid
    - add data-testid to ErrorPage404

--- packages/core-design/src/components/ErrorPage404/ErrorPage404.tsx
+++ packages/core-design/src/components/ErrorPage404/ErrorPage404.tsx
@@ -10,28 +10,32 @@ import styles from './ErrorPage404.styles'
 
 const ErrorPage404: React.FC<Props> = ({ className, title, subTitle, Img, children }) => {
   return (
-    <Grid className={className}>
-      <div className="sticky">
+    <Grid className={className} data-testid="ErrorPage404:block">
+      <div className="sticky" data-testid="ErrorPage404:block:sticky">
         <Heading
           as="h1"
           className="title"
           type="slab"
           size="l"
           dangerouslySetInnerHTML={{ __html: title }}
+          data-testid="ErrorPage404:text:title"
         />
 
         {Img && <Img className="img-wrap" />}
       </div>
 
-      <div className="content">
+      <div className="content" data-testid="ErrorPage404:block:content">
         <SubHeading
           as="p"
           className="subTitle"
           type="regular"
           dangerouslySetInnerHTML={{ __html: subTitle }}
+          data-testid="ErrorPage404:text:subTitle"
         />
 
-        <div className="children">{children}</div>
+        <div className="children" data-testid="ErrorPage404:block:children">
+          {children}
+        </div>
       </div>
     </Grid>
   )
