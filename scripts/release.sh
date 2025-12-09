6d3a1eab10df8618ecdd604f08c298ea417a44e5
Aleksandr Krutikov <alex.krutikov@gmail.com>
2025-12-09 16:22:00 +0100

    Switch to trusted publish on npm

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-printf "//registry.npmjs.org/:_authToken="%s"\n@csssr:registry=https://registry.npmjs.org/\n" "$NPM_TOKEN" >> $HOME/.npmrc 2> /dev/null
 ./node_modules/.bin/lerna publish from-git --yes --registry https://registry.npmjs.org/ --no-push
