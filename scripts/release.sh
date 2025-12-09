9d7b97ae65c40493aecf13d5dd573c36ea10a7a8
Aleksandr Krutikov <alex.krutikov@gmail.com>
2025-12-09 16:27:52 +0100

    Debug publish

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-./node_modules/.bin/lerna publish from-git --yes --registry https://registry.npmjs.org/ --no-push
+export NPM_CONFIG_PROVENANCE=${NPM_CONFIG_PROVENANCE:-true}
+export NPM_CONFIG_REGISTRY=${NPM_CONFIG_REGISTRY:-https://registry.npmjs.org/}
+export NPM_CONFIG_ACCESS=${NPM_CONFIG_ACCESS:-public}
+
+./node_modules/.bin/lerna publish from-git --yes --registry "$NPM_CONFIG_REGISTRY" --no-push --no-verify-access
